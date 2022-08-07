949b328523d7a8bc7d2649a0722edc85714bb688
Weebstonstein <mihneacercel@gmail.com>
2022-08-07 22:53:05 +0300

    Revert "Update Cod"
    This reverts commit 456a665904f345cd2c528d5c60d1c4578756e328.

--- My project/Assets/movement.cs
+++ My project/Assets/movement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 public class movement : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [Header("Referinte Obiecte")]
     public Transform groundcheck;
     public Transform frontcheck;
@@ -11,9 +10,6 @@ public class movement : MonoBehaviour
     public Rigidbody2D rb;
 
     [Header("Jump")]
-=======
-    private float h;
->>>>>>> Stashed changes
     public float Speed = 5f;
     public bool isGrounded;
     private bool isjumping;
@@ -22,7 +18,6 @@ public class movement : MonoBehaviour
     public float jumpstarttime;
     public float gravityIncreasePerSecond = 0.8f;
 
-<<<<<<< Updated upstream
     [Header("Wall Slide")]
     public bool space;
     public int isfacingright;
@@ -34,31 +29,13 @@ public class movement : MonoBehaviour
     public float walljumptime;
     public float xWallforce;
     public float yWallforce;
-=======
-    private bool isDashing = false;
-    private bool canDash = true;
-    private float dashingPower = 12f;
-    private float dashingTime = 0.2f;
-    private float dashingCooldown = 1f;
-    private bool isFacingRight = true;
-
-
->>>>>>> Stashed changes
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-<<<<<<< Updated upstream
         if (!isGrounded && !wallsliding)
-=======
-        if(isDashing) //cat timp caracterul e in dash nu poate face nimic altceva
-        {
-            return;
-        }
-        if (!isGrounded)
->>>>>>> Stashed changes
             rb.gravityScale += gravityIncreasePerSecond * Time.deltaTime;
         else rb.gravityScale = 1;
         if (!isjumping)
@@ -86,7 +63,6 @@ public class movement : MonoBehaviour
 
         }
         if (Input.GetKeyUp(KeyCode.Space)) //cand nu se mai apasa space
-<<<<<<< Updated upstream
         {
             isjumping = false;
             
@@ -97,16 +73,6 @@ public class movement : MonoBehaviour
         if (isGrounded)
         { wallsliding = false; }
 
-=======
-            {
-                isjumping = false;
-            }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) //cand se apasa shift si caracterul nu este in dash
-        {
-            StartCoroutine(dash());
-        }
-        Flip();//apeleaza functie flip pt a intoarce caracterul daca e nevoie
->>>>>>> Stashed changes
 
 
         if (Input.GetButtonDown("Jump"))
@@ -131,7 +97,6 @@ public class movement : MonoBehaviour
 
     void FixedUpdate()
     {
-<<<<<<< Updated upstream
   
         float h = Input.GetAxisRaw("Horizontal");
         if (h < 0)
@@ -144,13 +109,6 @@ public class movement : MonoBehaviour
          isfacingright =1;
             transform.localScale = new Vector2(1, transform.localScale.y);
         }
-=======
-        if(isDashing) //cat timp caracterul e in dash nu poate face nimic altceva
-        {
-            return;
-        }
-        h = Input.GetAxisRaw("Horizontal");
->>>>>>> Stashed changes
         rb.velocity = new Vector2(h * Speed, rb.velocity.y); //miscare stanga-dreapta
 
         //wall slide
@@ -185,36 +143,5 @@ public class movement : MonoBehaviour
         walljumping = false;
     }
 
-<<<<<<< Updated upstream
 
 }
-=======
-    void OnCollisionExit2D(Collision2D col)
-    { isGrounded = false; }
-
-    private IEnumerator dash() //cod pt dash
-    {
-        canDash = false;
-        isDashing = true;
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-        yield return new WaitForSeconds(dashingTime);
-        rb.gravityScale = originalGravity;
-        isDashing = false;
-        yield return new WaitForSeconds(dashingCooldown);
-        canDash = true;
-    }
-
-    private void Flip() //intoarce caracterul in functie de directia in care mergi
-    {
-        if(isFacingRight && h < 0f || !isFacingRight && h > 0f) 
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-    }
-}
->>>>>>> Stashed changes
